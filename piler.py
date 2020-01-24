43e2316bb40f21d8b3196f78df309bc9ed9eb851
infrub <infrub@gmail.com>
2020-01-24 23:18:58 +0900

    bioperator

--- piler.py
+++ piler.py
@@ -1,12 +1,21 @@
 from lark import Lark
+from lark.indenter import Indenter
 
 
 program = open('source01.tnk').read()
 rule = open('grammer.txt').read()
 
+class TreeIndenter(Indenter):
+    NL_type = '_NL'
+    OPEN_PAREN_types = []
+    CLOSE_PAREN_types = []
+    INDENT_type = '_INDENT'
+    DEDENT_type = '_DEDENT'
+    tab_len = 4
 
-parser = Lark(rule, start='program', parser='lalr')
+parser = Lark(rule, start='program', parser='lalr', postlex=TreeIndenter())
 tree = parser.parse(program)
 
 
 print(tree)
+print(tree.pretty())
